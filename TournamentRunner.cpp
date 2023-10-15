47d100aeea622f272a84d48e305adfac8b53b816
VisibleReality <visible@duck.com>
2023-10-15 20:59:43 +1100

    Minor changes to TournamentRunner

--- TournamentRunner.cpp
+++ TournamentRunner.cpp
@@ -4,6 +4,7 @@
 #include "TournamentRunner.h"
 #include <vector>
 #include <thread>
+//#include <iostream>
 
 TournamentRunner::TournamentRunner(Tournament& tournament, PlatypusGame& platypusGame, unsigned int threadCount) :
 		tournament {tournament}, platypusGame {platypusGame}, threadCount {threadCount}
@@ -12,6 +13,7 @@ TournamentRunner::TournamentRunner(Tournament& tournament, PlatypusGame& platypu
 
 void TournamentRunner::playGames()
 {
+//	std::cout << "Starting thread" << std::endl;
 	std::vector<std::shared_ptr<std::vector<unsigned long>>> games;
 	std::vector<std::shared_ptr<PlatypusGame::Result>> results;
 
@@ -20,6 +22,13 @@ void TournamentRunner::playGames()
 		games = tournament.getNextGames(BATCH_SIZE);
 		results.clear();
 
+		if (games.empty())
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+
+//		std::cout << "Got " << games.size() << " games" << std::endl;
+
 		for (const auto& game : games)
 		{
 			results.push_back(platypusGame.runGame(*game));
@@ -27,21 +36,28 @@ void TournamentRunner::playGames()
 
 		tournament.processResults(results);
 	}
+//	std::cout << "Ending thread" << std::endl;
 }
 
 void TournamentRunner::runTournament()
 {
-	std::vector<std::thread> threads;
-
-	threads.reserve(threadCount);
-	for (int i = 0; i < threadCount; ++i)
+	if (threadCount > 1)
 	{
-		threads.emplace_back(&TournamentRunner::playGames, this);
-	}
+		std::vector<std::thread> threads;
 
-	// Wait for threads to finish
-	for (auto& thread : threads)
+		threads.reserve(threadCount);
+		for (int i = 0; i < threadCount; ++i)
+		{
+			threads.emplace_back(&TournamentRunner::playGames, this);
+		}
+
+		// Wait for threads to finish
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+	} else // Don't bother creating another thread if only 1 thread is needed
 	{
-		thread.join();
+		playGames();
 	}
 }
