6b0e56f9ab3ba9f4ecc1e57a19293075e4983c49
VisibleReality <visible@duck.com>
2023-10-16 00:40:18 +1100

    Added argparse for a functional CLI

--- main.cpp
+++ main.cpp
@@ -3,16 +3,41 @@
 #include <thread>
 #include <fstream>
 #include <algorithm>
-#include <cctype>
+#include "include/argparse/argparse.hpp"
 #include "PlatypusGame.h"
 #include "RoundRobinTournament.h"
 #include "TournamentRunner.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	argparse::ArgumentParser program("PlatypusGame");
+
+	program.add_argument("tournament-type").help("Name of tournament to run. Valid names are: roundrobin");
+	program.add_argument("input").help("File to read machine numbers from");
+	program.add_argument("output").help("Filename to write results to");
+
+	program.add_argument("--rules")
+			.help("List of rules to add to the platypus game. Valid rules are: short long tree tiebreaker terminate")
+			.nargs(argparse::nargs_pattern::at_least_one);
+	program.add_argument("--game-length")
+			.help("Set custom game length, in turns")
+			.scan<'d', int>();
+
+	try
+	{
+		program.parse_args(argc, argv);
+	}
+	catch (const std::runtime_error& err)
+	{
+		std::cerr << err.what() << std::endl;
+		std::cerr << program;
+		std::exit(1);
+	}
+
+
 	std::vector<unsigned long> players;
 
-	std::ifstream inFile("players.txt");
+	std::ifstream inFile(program.get<std::string>("input"));
 	std::string line;
 
 	while (std::getline(inFile, line))
@@ -26,25 +51,79 @@ int main()
 		}
 	}
 
+	if (players.empty())
+	{
+		std::cerr << "Error: Could not load any players" << std::endl;
+		std::exit(1);
+	}
+
 	std::cout << "Loaded " << players.size() << " players" << std::endl;
 
-	RoundRobinTournament tournament = RoundRobinTournament(players);
-
-	PlatypusGame platypusGame = PlatypusGame(
-			{PlatypusGame::Rule::TREE, PlatypusGame::Rule::TERMINATE, PlatypusGame::Rule::TIEBREAKER});
-
-	TournamentRunner runner = TournamentRunner(tournament, platypusGame,
-											   std::thread::hardware_concurrency());
+	std::vector<PlatypusGame::Rule> rules;
+	if (program.present("--rules"))
+	{
+		std::cout << "Using rules: ";
+		for (const auto& rule : program.get<std::vector<std::string>>("--rules"))
+		{
+			if (rule == "long")
+			{
+				rules.push_back(PlatypusGame::Rule::LONG);
+				std::cout << "long" ;
+			}
+			else if (rule == "short")
+			{
+				rules.push_back(PlatypusGame::Rule::SHORT);
+				std::cout << "short ";
+			}
+			else if (rule == "tree")
+			{
+				rules.push_back(PlatypusGame::Rule::TREE);
+				std::cout << "tree ";
+			}
+			else if (rule == "tiebreaker")
+			{
+				rules.push_back(PlatypusGame::Rule::TIEBREAKER);
+				std::cout << "tiebreaker ";
+			}
+			else if (rule == "terminate")
+			{
+				rules.push_back(PlatypusGame::Rule::TERMINATE);
+				std::cout << "terminate ";
+			}
+		}
+		std::cout << std::endl;
+	}
 
 	auto startTime = std::chrono::high_resolution_clock::now();
+
+	PlatypusGame platypusGame;
+	if (program.present("--game-length"))
+	{
+		platypusGame = PlatypusGame(rules, program.get<int>("--game-length"));
+		std::cout << "Using game length: " << program.get<int>("--game-length") << std::endl;
+	}
+	else
+	{
+		platypusGame = PlatypusGame(rules);
+	}
+
+	std::shared_ptr<Tournament> tournament;
+
+	if (program.get<std::string>("tournament-type") == "roundrobin")
+	{
+		tournament = std::make_shared<RoundRobinTournament>(players);
+	}
+
+	TournamentRunner runner = TournamentRunner(*tournament, platypusGame,
+											   std::thread::hardware_concurrency());
 
 	runner.runTournament();
 
 	auto finishTime = std::chrono::high_resolution_clock::now();
 
-	std::ofstream outFile("results.csv");
+	std::ofstream outFile(program.get<std::string>("output"));
 
-	tournament.outputResults(outFile);
+	tournament->outputResults(outFile);
 
 	std::chrono::duration<double, std::milli> duration = finishTime - startTime;
 
