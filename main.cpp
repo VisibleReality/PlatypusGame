ee3f4c7bd665baef54edb39bfcc0ddab829f6190
VisibleReality <visible@duck.com>
2023-10-15 20:59:12 +1100

    Add basic code to run a tournament

--- main.cpp
+++ main.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <fstream>
+#include <algorithm>
+#include <cctype>
 #include "PlatypusGame.h"
-#include "Tournament.h"
 #include "RoundRobinTournament.h"
+#include "TournamentRunner.h"
 
 int main()
 {
 	std::vector<unsigned long> players;
 
-	players.push_back(1);
-	players.push_back(2);
-	players.push_back(3);
-	players.push_back(4);
+	std::ifstream inFile("players.txt");
+	std::string line;
+
+	while (std::getline(inFile, line))
+	{
+		line.erase(std::remove_if(line.begin(), line.end(),
+								  [](auto const& c) -> bool { return !std::isdigit(c); }),
+				   line.end());
+		if (!line.empty())
+		{
+			players.push_back(std::stoi(line));
+		}
+	}
+
+	std::cout << "Loaded " << players.size() << " players" << std::endl;
 
 	RoundRobinTournament tournament = RoundRobinTournament(players);
 
-	auto games = tournament.getNextGames(9);
-	std::cout << "set1" << std::endl;
-	for (const auto& game : games)
-	{
-		std::cout << game->at(0) << " " << game->at(1) << std::endl;
-	}
+	PlatypusGame platypusGame = PlatypusGame();
 
-	std::cout << tournament.tournamentDone() << std::endl;
+	TournamentRunner runner = TournamentRunner(tournament, platypusGame,
+											   std::thread::hardware_concurrency());
 
-	games = tournament.getNextGames(9);
-	std::cout << "set2" << std::endl;
-	for (const auto& game : games)
-	{
-		std::cout << game->at(0) << " " << game->at(1) << std::endl;
-	}
+	auto startTime = std::chrono::high_resolution_clock::now();
 
-	std::cout << tournament.tournamentDone() << std::endl;
+	runner.runTournament();
 
-//	PlatypusGame::printTable(167902255);
+	auto finishTime = std::chrono::high_resolution_clock::now();
+
+	std::ofstream outFile("results.csv");
+
+	tournament.outputResults(outFile);
+
+	std::chrono::duration<double, std::milli> duration = finishTime - startTime;
+
+	std::cout << "Took " << duration.count() / 1000 << " seconds" << std::endl;
 
 	return 0;
 }
